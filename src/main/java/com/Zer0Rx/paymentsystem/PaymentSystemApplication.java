fa32b8f85376351f1784f0719558c267c16dd17f
Natan <zerogamerptbr@gmail.com>
2024-02-17 13:44:19 -0300

    :tada: initial commit

---
+++ src/main/java/com/Zer0Rx/paymentsystem/PaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Zer0Rx.paymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Zer0Rx/paymentsystem/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zer0Rx.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
