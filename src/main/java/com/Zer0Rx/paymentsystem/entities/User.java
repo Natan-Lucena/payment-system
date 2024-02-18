4a9cc9af928bb6912fcbb46f5e0d2583ef45e562
Natan <zerogamerptbr@gmail.com>
2024-02-18 01:02:49 -0300

    :sparkles: feat: add user entity and userRepository

---
+++ src/main/java/com/Zer0Rx/paymentsystem/entities/User.java
@@ -0,0 +1,65 @@
+package com.Zer0Rx.paymentsystem.entities;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter()
+@Setter()
+@AllArgsConstructor()
+@NoArgsConstructor()
+@Entity(name = "users")
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String verificationCode;
+    private boolean enable;
+
+    public User(String name, String email, String password){
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+
+    }
+    @Override
+    public boolean isEnabled() {
+        return this.enable;
+    }
+}
---
+++ src/main/java/com/Zer0Rx/paymentsystem/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Zer0Rx.paymentsystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.Zer0Rx.paymentsystem.entities.User;;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
+}
