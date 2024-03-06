77fd7d7096b6b447cd6c220be50b7c62d2b26a55
Natan <zerogamerptbr@gmail.com>
2024-03-06 20:51:21 -0300

    feat: setup of securityFilter

--- src/main/java/com/Zer0Rx/paymentsystem/config/security/SecurityConfig.java
+++ src/main/java/com/Zer0Rx/paymentsystem/config/security/SecurityConfig.java
@@ -3,15 +3,33 @@ package com.Zer0Rx.paymentsystem.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Autowired
     private SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChan(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+        .requestMatchers(HttpMethod.POST, "/user/register")
+        .permitAll().requestMatchers(HttpMethod.GET, "user/verify")
+        .permitAll().anyRequest().authenticated())
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
+    } 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
--- src/main/java/com/Zer0Rx/paymentsystem/config/security/SecurityFilter.java
+++ src/main/java/com/Zer0Rx/paymentsystem/config/security/SecurityFilter.java
@@ -27,7 +27,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @SuppressWarnings("null")
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, @SuppressWarnings("null") FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
                 var token = this.recoveryToken(request);
                 if(token != null){
