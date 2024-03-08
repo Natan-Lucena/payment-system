7bfcd2a38fd017f3948c77464016d1c745ddeb53
Natan <zerogamerptbr@gmail.com>
2024-03-08 09:12:03 -0300

    :sparkles: feat: add authenticationManager bean

--- src/main/java/com/Zer0Rx/paymentsystem/config/security/SecurityConfig.java
+++ src/main/java/com/Zer0Rx/paymentsystem/config/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,7 +31,10 @@ public class SecurityConfig {
         .permitAll().anyRequest().authenticated())
         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
-    } 
+    }
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
