901495d559cfe53db1258285612479afa9db06bc
Natan <zerogamerptbr@gmail.com>
2024-04-08 19:16:32 -0300

    fix: solving dependency injection and request mapping error

--- src/main/java/com/Zer0Rx/paymentsystem/controllers/LoginController.java
+++ src/main/java/com/Zer0Rx/paymentsystem/controllers/LoginController.java
@@ -7,13 +7,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.Zer0Rx.paymentsystem.dtos.AuthenticationDTO;
 import com.Zer0Rx.paymentsystem.dtos.AuthenticationResponseDTO;
 import com.Zer0Rx.paymentsystem.entities.User;
 import com.Zer0Rx.paymentsystem.services.TokenService;
+@RestController
 @RequestMapping("/user")
-
 public class LoginController {
     @Autowired
     private TokenService tokenService;
