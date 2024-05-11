a6d1a511272cc6e055d1faf177a9ac35d50fbf35
Natan Lucena <zerogamerptbr@gmail.com>
2024-05-11 19:40:44 -0300

    :bug: fix: fixing security filter error treatment

--- src/main/java/com/Zer0Rx/paymentsystem/config/security/SecurityFilter.java
+++ src/main/java/com/Zer0Rx/paymentsystem/config/security/SecurityFilter.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.Zer0Rx.paymentsystem.config.exceptions.InvalidTokenException;
 import com.Zer0Rx.paymentsystem.repositories.UserRepository;
 import com.Zer0Rx.paymentsystem.services.TokenService;
 
@@ -41,28 +39,17 @@ public class SecurityFilter extends OncePerRequestFilter {
                         var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
                         SecurityContextHolder.getContext().setAuthentication(authentication);
                     }
-                } catch (InvalidTokenException e) {
-                        if (!response.isCommitted()) {
-                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                            response.setContentType("application/json");
-                            JSONObject res = new JSONObject();
-                            res.put("msg", e.getMessage());
-                            response.getWriter().write(res.toString());
-                            response.getWriter().flush();
-                        }
-                    } catch(AccessDeniedException e){
-                        if (!response.isCommitted()) {
-                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                            response.setContentType("application/json");
-                            JSONObject res = new JSONObject();
-                            res.put("msg", e.getMessage());
-                            response.getWriter().write(res.toString());
-                            response.getWriter().flush();
-                    }
+                } catch (RuntimeException e) {
+                    JSONObject res = new JSONObject();
+                    res.put("msg", e.getMessage());
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write(res.toString());
+                    response.getWriter().flush();
+                    return;
                 }
                 filterChain.doFilter(request, response);
             }
-    
+            
     private String recoveryToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
         if(authHeader == null){
